3643713de39a431be82fca4ede75d32913f0ac28
jamalsiam <jamalsiam1995@gmail.com>
2017-09-16 11:58:26 +0300

    add like in schema

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,68 +5,75 @@ var SALT_WORK_FACTOR = 10;
 
 // use schema to add it to mongo data base
 var UserSchema = new mongoose.Schema({
-  name:{
-    type: String,
-    required: true,
+	name:{
+		type: String,
+		required: true,
     // unique: true
-  },
-  mobile:{
-    type: String,
+},
+mobile:{
+	type: String,
     //required: true,
     //unique: true
-  },
-  email:{
-    type: String,
-    required: true,
-    unique: true
-  },
-  password:{
-    type: String,
-    required: true,
+},
+email:{
+	type: String,
+	required: true,
+	unique: true
+},
+password:{
+	type: String,
+	required: true,
     // unique: true
-  },
-  image:{
-    type: String,
+},
+image:{
+	type: String,
     //required: true,
     // unique: true
-  },
-  status:{
-    type: String,
+},
+status:{
+	type: String,
     //required: true,
     // unique: true
-  },
-  post:[
-  {
-    postText:{
-      type:String
-    },
-    image:{
-      type:String
-    },
-    
-    lat:{
-      type:String
-    },
-    lang:{
-      type:String
-    }  
-  }],
-  following:[
-  {
-    email:{
-      type:String,
+},
+post:[
+{
+	postText:{
+		type:String
+	},
+	image:{
+		type:String
+	},
+	
+	lat:{
+		type:String
+	},
+	lang:{
+		type:String
+	},
+	like:[
+	{
+		email:{
+			type:String
+		}
+	}
+	]  
+}],
+following:[
+{
+	email:{
+		type:String,
       //required: true,
       unique: true
-    } 
-  }],
-  follower:[
-  {
-    email:{
-      type:String,
+  } 
+}],
+follower:[
+{
+	email:{
+		type:String,
       //required: true,
       unique: true
-    } 
-  }]
+  } 
+}]
 
 });
 
@@ -74,43 +81,43 @@ var UserSchema = new mongoose.Schema({
 
 
 UserSchema.methods.comparePasswords = function (candidatePassword) {
-  var savedPassword = this.password;
-  return Q.Promise(function (resolve, reject) {
-    bcrypt.compare(candidatePassword, savedPassword, function (err, isMatch) {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(isMatch);
-      }
-    });
-  });
+	var savedPassword = this.password;
+	return Q.Promise(function (resolve, reject) {
+		bcrypt.compare(candidatePassword, savedPassword, function (err, isMatch) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(isMatch);
+			}
+		});
+	});
 };
 
 UserSchema.pre('save', function (next) {
-  var user = this;
+	var user = this;
 
 // only hash the password if it has been modified (or is new)
 if (!user.isModified('password')) {
-  return next();
+	return next();
 }
 
 // generate a salt
 bcrypt.genSalt(SALT_WORK_FACTOR, function (err, salt) {
-  if (err) {
-    return next(err);
-  }
+	if (err) {
+		return next(err);
+	}
 
   // hash the password along with our new salt
   bcrypt.hash(user.password, salt, null, function (err, hash) {
-    if (err) {
-      return next(err);
-    }
+  	if (err) {
+  		return next(err);
+  	}
 
     // override the cleartext password with the hashed one
     user.password = hash;
     user.salt = salt;
     next();
-  });
+});
 });
 });
 
